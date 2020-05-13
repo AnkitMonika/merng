772db2ca2f889ed5f753db60ee2ac1bbb712c918
Ankit_Sharma28 <Ankit_Sharma28@Dell.com>
2020-05-13 20:50:42 +0530

    modified app.js

--- client/src/index.js
+++ client/src/index.js
@@ -1,29 +1,11 @@
-import React from 'react'
+import React from 'react';
 import ReactDOM from 'react-dom';
-import {ApolloProvider} from '@apollo/react-hooks'
-import ApolloClient from 'apollo-client' 
-import {InMemoryCache} from 'apollo-cache-inmemory'
-import {createHttpLink} from 'apollo-link-http'
-import App from './App'
-
-//Apollo serevr uri
-const httpLink=createHttpLink({
-  uri:'http://localhost:5000'
-});
-
-//create httpLink and inMemoryCache
-const client=new ApolloClient({
-  link:httpLink,
-  cache:new InMemoryCache()
-});
+import App from './App';
 
 
 ReactDOM.render(
   <React.StrictMode>
-    <ApolloProvider client={client}>
-      <App/>
-    </ApolloProvider>
+    <App />
   </React.StrictMode>,
   document.getElementById('root')
 );
-
